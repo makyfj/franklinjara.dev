fab38d63f230c4455b49f6c41bf968a9911f34bf
Maky <franklinjaradev@gmail.com>
2022-01-22 18:22:36 -0500

    chore: adding toolkit items

--- src/components/toolkit.tsx
+++ src/components/toolkit.tsx
@@ -1,4 +1,35 @@
 import { FcSettings, FcSupport } from "react-icons/fc"
+import {
+  SiNextdotjs,
+  SiNeovim,
+  SiTypescript,
+  SiJavascript,
+  SiReact,
+  SiPrettier,
+  SiHtml5,
+  SiTailwindcss,
+  SiCss3,
+  SiVercel,
+  SiPython,
+  SiRedux,
+  SiChakraui,
+  SiMongodb,
+  SiExpress,
+  SiNodedotjs,
+  SiMysql,
+  SiPostgresql,
+  SiHeroku,
+  SiCplusplus,
+  SiJava,
+  SiNestjs,
+  SiGraphql,
+  SiApollographql,
+  SiGithub,
+  SiGit,
+  SiEslint,
+} from "react-icons/si"
+
+import ToolkitItem from "./toolkitItem"
 
 const Toolkit = () => {
   return (
@@ -12,9 +43,39 @@ const Toolkit = () => {
         I use a variety of tools to get through all the obstacles as efficiently
         as possible. I really enjoy working with statically typed languages,
         especially TypeScript. This negates most of the silly bugs that could
-        sneak into your codebase and creates a quick feedback loop to fix all
-        the little bugs as you write and refactor new code.
+        sneak into the codebase and creates a quick feedback loop to fix all the
+        little bugs as you write and refactor new code.
       </p>
+      <div className="grid grid-cols-3 sm:grid-cols-5 gap-4 max-w-sm sm:max-w-lg mx-auto bg-slate-700 dark:bg-slate-300 text-slate-300 dark:text-slate-700 p-2 rounded shadow shadow-xl">
+        <ToolkitItem name="NextJS" icon={SiNextdotjs} />
+        <ToolkitItem name="Neovim" icon={SiNeovim} />
+        <ToolkitItem name="TypeScript" icon={SiTypescript} />
+        <ToolkitItem name="JavaScript" icon={SiJavascript} />
+        <ToolkitItem name="React" icon={SiReact} />
+        <ToolkitItem name="Prettier" icon={SiPrettier} />
+        <ToolkitItem name="HTML5" icon={SiHtml5} />
+        <ToolkitItem name="Tailwindcss" icon={SiTailwindcss} />
+        <ToolkitItem name="CSS3" icon={SiCss3} />
+        <ToolkitItem name="Vercel" icon={SiVercel} />
+        <ToolkitItem name="Python" icon={SiPython} />
+
+        <ToolkitItem name="Git" icon={SiGit} />
+        <ToolkitItem name="SiGithub" icon={SiGithub} />
+        <ToolkitItem name="Apollo" icon={SiApollographql} />
+        <ToolkitItem name="GraphQL" icon={SiGraphql} />
+        <ToolkitItem name="NestJS" icon={SiNestjs} />
+        <ToolkitItem name="Java" icon={SiJava} />
+        <ToolkitItem name="C++" icon={SiCplusplus} />
+        <ToolkitItem name="Heroku" icon={SiHeroku} />
+        <ToolkitItem name="Redux" icon={SiRedux} />
+        <ToolkitItem name="Chakra" icon={SiChakraui} />
+        <ToolkitItem name="MongoDB" icon={SiMongodb} />
+        <ToolkitItem name="Express" icon={SiExpress} />
+        <ToolkitItem name="NodeJS" icon={SiNodedotjs} />
+        <ToolkitItem name="MySQL" icon={SiMysql} />
+        <ToolkitItem name="Eslint" icon={SiEslint} />
+        <ToolkitItem name="PostgreSQL" icon={SiPostgresql} />
+      </div>
     </>
   )
 }
--- src/components/toolkitItem.tsx
+++ src/components/toolkitItem.tsx
@@ -1,5 +1,17 @@
-const ToolkitItem = () => {
-  return <div></div>
+import type { IconType } from "react-icons"
+
+interface ToolkitItemProps {
+  icon: IconType
+  name: string
+}
+
+const ToolkitItem = ({ icon, name }: ToolkitItemProps) => {
+  return (
+    <div className="flex space-x-2 justify-center items-center">
+      <span>{icon({ className: "w-6 h-6" })}</span>
+      <p className="text-sm">{name}</p>
+    </div>
+  )
 }
 
 export default ToolkitItem
