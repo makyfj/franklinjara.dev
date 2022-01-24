6d15e60f4a47742f07c278a3ad476ae320f53dc7
Maky <franklinjaradev@gmail.com>
2022-01-24 08:16:19 -0500

    fix: fixing Head title for blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,8 +1,12 @@
 import { FcSupport, FcSettings } from "react-icons/fc"
+import Head from "next/head"
 
 const Blog = () => {
   return (
     <>
+      <Head>
+        <title>Blog | FJ</title>
+      </Head>
       <div className="flex justify-center items-center gap-1">
         <h1 className="text-lg sm:text-xl text-center">Coming soon...</h1>
         <FcSupport className="text-4xl" />
