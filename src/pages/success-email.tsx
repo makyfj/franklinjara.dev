46c6674536319d4bc43736ec310f39e1394dd3a0
Maky <franklinjaradev@gmail.com>
2022-01-24 15:57:27 -0500

    chore: Head and animation for success-email page

--- src/pages/success-email.tsx
+++ src/pages/success-email.tsx
@@ -1,14 +1,45 @@
+import Head from "next/head"
+import { motion } from "framer-motion"
+
+import { itemVariants } from "@/utils/animation"
+
 const SuccessEmail = () => {
   return (
-    <div className="flex flex-col justify-center items-center py-10 gap-5">
-      <h1>Thank you!</h1>
-      <p>
-        I{`'`}ll contact you as soon as possible. Feel free to checkout my
-        projects on GitHub. Thanks for reaching out 😉
-      </p>
-      <p>- Franklin</p>
-    </div>
-  );
-};
+    <>
+      <Head>
+        <title>Thank you!</title>
+      </Head>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <motion.h1
+          custom={1 * 0.2}
+          variants={itemVariants}
+          initial="hidden"
+          animate="visible"
+          className="text-lg"
+        >
+          Thank you!
+        </motion.h1>
+        <motion.p
+          custom={1 * 0.3}
+          variants={itemVariants}
+          initial="hidden"
+          animate="visible"
+          className="max-w-md text-center"
+        >
+          I{`'`}ll contact you as soon as possible. Feel free to checkout my
+          projects on GitHub. Thanks for reaching out 😉
+        </motion.p>
+        <motion.p
+          custom={1 * 0.4}
+          variants={itemVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          - Franklin
+        </motion.p>
+      </div>
+    </>
+  )
+}
 
-export default SuccessEmail;
+export default SuccessEmail
