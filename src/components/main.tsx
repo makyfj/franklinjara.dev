f4dc9656439dcad255168a0fc55fcd7e26a294e0
Franklin Jara <franklinjaradev@gmail.com>
2022-06-26 21:46:29 -0400

    :bento: Update profile
    Image for profile updated

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -25,7 +25,7 @@ const Main = () => {
       <div className="flex flex-col items-center gap-4 my-2">
         <div>
           <Image
-            src="/images/profile.jpg"
+            src="/images/profile.jpeg"
             alt="Profile image"
             width={200}
             height={200}
