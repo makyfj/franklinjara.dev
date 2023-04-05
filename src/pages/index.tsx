ef6125fff0ad4eb7d605604d1731085d10410ebf
Franklin Jara <franklinjaradev@gmail.com>
2023-04-04 22:57:55 -0400

    :sparkles: feat: adding image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import Image from "next/image"
 
 import Socials from "@/components/socials"
 import Toolkit from "@/components/toolkit"
@@ -17,14 +18,34 @@ const Home = () => {
         "
         />
         <meta name="title" content="Franklin Jara" />
+        <meta property="og:image" content="/images/franklin.webp" key="image" />
         <meta
           name="keywords"
-          content="Franklin, Jara, Software, Engineering, Full Stack Developer, Engineer"
+          content="Franklin, Jara, Software, Engineering, Full Stack Developer, Engineer, Franklin Jara"
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@iwanttobeinmars" />
+        <meta name="twitter:title" content="Franklin Jara" />
+        <meta
+          name="twitter:description"
+          content="
+          Franklin Jara is a Full Stack Developer @ Cherry Hill Programs and Student @ Adelphi University pursuing a Master's Degree in
+          Computer Science with specialization in Software Engineering. 
+        "
+        />
+
+        <meta name="twitter:image" content="/images/franklin.webp" />
       </Head>
       <div className="flex flex-col items-center gap-4 my-2">
         <div className="flex flex-col items-center justify-between gap-4">
+          <Image
+            src="/images/franklin.webp"
+            width={180}
+            height={180}
+            alt="Franklin Jara"
+            className="rounded-full"
+          />
           <h1 className="text-3xl font-bold text-center">Franklin Jara</h1>
           <Socials />
         </div>
